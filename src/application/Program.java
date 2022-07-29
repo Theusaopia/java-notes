dec949cfd81b0bce1684c82ae004f0d2899c196f
Matheus Dias <matheusdyas007@hotmail.com>
2022-07-29 19:21:11 -0300

    map

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,50 +1,27 @@
 package application;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-/**
- * In an online courses portal, each user has a unique code, represented by an integer number.
- *
- * Each instructor of the portal can have many courses, and a same student can enroll in many courses. Thus, the
- * total number of an instructor students isn't simply the sum of all the students of his courses, because there
- * aren't repeated students in more than one course.
- *
- * Alex have 3 courses, A, B and C and wants to know his total number of students.
- *
- * The program must read the students of courses A, B and C of Alex and after that, show the
- * total number of alex students.
- * **/
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Program {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Map<String, String> cookies = new TreeMap<>();
 
-        Set<Integer> courseA = new HashSet<>();
-        Set<Integer> courseB = new HashSet<>();
-        Set<Integer> courseC = new HashSet<>();
+        cookies.put("username", "Maria");
+        cookies.put("email", "Maria@gmail.com");
+        cookies.put("phone", "34932840238");
 
-        populateSet(courseA, "course A", sc);
-        populateSet(courseB, "course B", sc);
-        populateSet(courseC, "course C", sc);
+        cookies.remove("email");
+        cookies.put("phone", "1122");
 
-        Set<Integer> total = new HashSet<>();
-        total.addAll(courseA);
-        total.addAll(courseB);
-        total.addAll(courseC);
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
 
-        System.out.println("Total students: " + total.size());
-
-        sc.close();
-    }
-
-    public static void populateSet(Set<Integer> set, String courseName, Scanner sc) {
-        System.out.println("How many students for " + courseName + "?");
-        int numberOfStudents = sc.nextInt();
-
-        for (int i = 0; i < numberOfStudents; i++) {
-            set.add(sc.nextInt());
+        System.out.println("All Cookies: ");
+        for(String key : cookies.keySet()) {
+            System.out.println(key + ": " + cookies.get(key));
         }
     }
 }
---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Map<Product, Double> cookies = new HashMap<>();
+
+        Product p1 = new Product("Tv", 900.0);
+        Product p2 = new Product("Notebook", 900.0);
+        Product p3 = new Product("Tablet", 400.0);
+
+        cookies.put(p1, 10000.0);
+        cookies.put(p2, 20000.0);
+        cookies.put(p3, 15000.0);
+
+        Product ps = new Product("Tv", 900.0);
+
+        System.out.println("Contains 'ps' key: " + cookies.containsKey(ps));
+
+    }
+}
