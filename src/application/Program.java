db5fa26a329718104982ce202aed63aa2c960ac6
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-05 20:16:39 -0300

    using predicates as function parameters

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,10 @@
 package application;
 
 import entities.Product;
-import util.UpperCaseName;
+import util.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,15 +15,10 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        Function<Product, String> func = product -> product.getName().toUpperCase();
+        ProductService ps = new ProductService();
 
+        double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 
-        //List<String> uppercase = list.stream().map(new UpperCaseName()).collect(Collectors.toList()); interface implementation
-        //List<String> uppercase = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList()); static method reference
-        //List<String> uppercase = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList()); non-static method reference
-        //List<String> uppercase = list.stream().map(func).collect(Collectors.toList()); declared lambda expression
-        List<String> uppercase = list.stream().map(product -> product.getName().toUpperCase()).collect(Collectors.toList());
-
-        uppercase.forEach(System.out::println);
+        System.out.println("Sum = " + String.format("%.2f", sum));
     }
 }
