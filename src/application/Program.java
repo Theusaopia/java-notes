161a9cbed098111c7c9068af17638f7d04838533
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-05 21:11:09 -0300

    example exercise (stream pipelines)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,48 @@
 package application;
 
-import java.util.Arrays;
-import java.util.List;
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        Stream<Integer> st1 = list.stream().map(x -> x * 10);
-        System.out.println(Arrays.toString(st1.toArray()));
+        System.out.println("Enter full file path: ");
+        String path = sc.nextLine();
 
-        //int sum = list.stream().reduce(0, (x, y) -> x + y);
-        int sum = list.stream().reduce(0, Integer::sum);
-        System.out.println(sum);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Product> list = new ArrayList<>();
 
-        List<Integer> newList = list.stream()
-                .filter(x -> x % 2 == 0)
-                .map(x -> x * 10)
-                .collect(Collectors.toList());
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
 
-        System.out.println(Arrays.toString(newList.toArray()));
+            double avg = list.stream()
+                    .map(Product::getPrice)
+                    .reduce(0.0, Double::sum) / list.size();
+
+            System.out.println("Average price: " + String.format("%.2f", avg));
+
+            Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                    .filter(p -> p.getPrice() < avg)
+                    .map(Product::getName)
+                    .sorted(comp.reversed())
+                    .collect(Collectors.toList());
+
+            names.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
