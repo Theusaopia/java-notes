ec0f4b0c55ccea0f05a79f54dd9ad1fc820d4c89
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-08 21:29:54 -0300

    retrieving data from the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,37 @@
 package application;
 
 import db.DB;
+import exceptions.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("SELECT * FROM department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }
+        catch (SQLException ex) {
+            throw new DbException(ex.getMessage());
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
