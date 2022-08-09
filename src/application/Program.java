884c2a545ec1deb2f8bb31cb623f18e405bac5d6
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-09 08:57:31 -0300

    transactions: commits and rollbacks

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,33 +2,45 @@ package application;
 
 
 import db.DB;
-import exceptions.DbIntegrityException;
+import exceptions.DbException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "DELETE FROM department " +
-                            "WHERE " +
-                            "Id = ?"
-            );
 
-            st.setInt(1, 2);
+            conn.setAutoCommit(false);
 
-            int rowsAffected = st.executeUpdate();
+            st = conn.createStatement();
 
-            System.out.println("Done! Affected Rows: " + rowsAffected);
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+//            int x = 1;
+//            if(x < 2) {
+//                throw new SQLException("Fake error");
+//            }
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1 " + rows1);
+            System.out.println("rows2 " + rows2);
         }
         catch (SQLException ex) {
-            throw new DbIntegrityException(ex.getMessage());
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back!! Caused by: " + ex.getMessage());
+            } catch (SQLException e) {
+                throw new DbException("Error trying to rollback! Caused by " + e.getMessage());
+            }
         }
         finally {
             DB.closeStatement(st);
