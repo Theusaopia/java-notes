a80d212906933e80f2ca47821bcc9da27820e441
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-13 20:48:44 -0300

    project DB: created implementation and factory of DAO's

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Boooks");
         Seller seller = new Seller(1, "Bob", "bob@email.com", new Date(), 2000.0, obj);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(seller);
