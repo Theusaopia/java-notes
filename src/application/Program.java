91ae1b861e0a68ec14742f2c72072e5ab34abb9e
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-13 21:05:34 -0300

    project DB: implementing findById for seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Boooks");
-        Seller seller = new Seller(1, "Bob", "bob@email.com", new Date(), 2000.0, obj);
-
         SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
+        Seller seller = sellerDAO.findById(3);
+
         System.out.println(seller);
     }
 }
